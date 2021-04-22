ce8aad93cc19eeec1f35818c3cff7d585de19db8
Marius Preikschat <m.preikschat@googlemail.com>
2021-04-22 18:35:17 +0200

    ui again

--- lib/screens/setNumberOfPlayers.dart
+++ lib/screens/setNumberOfPlayers.dart
@@ -6,6 +6,8 @@ class SetNumberOfPlayers extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var game = context.watch<Game>();
+    const paddingVertical = EdgeInsets.symmetric(vertical: 30.0);
+    const paddingHorizontal = EdgeInsets.symmetric(horizontal: 30.0);
 
     return Scaffold(
       appBar: AppBar(
@@ -16,40 +18,83 @@ class SetNumberOfPlayers extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            FloatingActionButton.extended(
-              heroTag: 1,
-              onPressed: () =>
-                  Navigator.popAndPushNamed(context, '/setPlayerNames'),
-              tooltip: '3 Spieler',
-              label: Text("3", style: Theme.of(context).textTheme.button),
-            ),
-            FloatingActionButton.extended(
-              heroTag: 2,
-              onPressed: () => {
-                game.setNumberOfPlayers(4),
-                Navigator.popAndPushNamed(context, '/setPlayerNames')
-              },
-              tooltip: '4 Spieler',
-              label: Text("4", style: Theme.of(context).textTheme.button),
-            ),
-            FloatingActionButton.extended(
-              heroTag: 3,
-              onPressed: () => {
-                game.setNumberOfPlayers(5),
-                Navigator.popAndPushNamed(context, '/setPlayerNames')
-              },
-              tooltip: '5 Spieler',
-              label: Text("5", style: Theme.of(context).textTheme.button),
-            ),
-            FloatingActionButton.extended(
-              heroTag: 4,
-              onPressed: () => {
-                game.setNumberOfPlayers(6),
-                Navigator.popAndPushNamed(context, '/setPlayerNames')
-              },
-              tooltip: '6 Spieler',
-              label: Text("6", style: Theme.of(context).textTheme.button),
-            ),
+            Padding(
+                padding: paddingVertical,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Padding(
+                        padding: paddingHorizontal,
+                        child: Transform.scale(
+                            scale: 2.0,
+                            child: FloatingActionButton.extended(
+                              heroTag: 1,
+                              hoverColor: Colors.red,
+                              onPressed: () => Navigator.popAndPushNamed(
+                                  context, '/setPlayerNames'),
+                              tooltip: '3 Spieler',
+                              label: Text("3",
+                                  style: Theme.of(context).textTheme.button),
+                            ))),
+                    Padding(
+                        padding: paddingHorizontal,
+                        child: Transform.scale(
+                            scale: 2.0,
+                            child: FloatingActionButton.extended(
+                              heroTag: 2,
+                              hoverColor: Colors.red,
+                              onPressed: () => {
+                                game.setNumberOfPlayers(4),
+                                Navigator.popAndPushNamed(
+                                    context, '/setPlayerNames')
+                              },
+                              tooltip: '4 Spieler',
+                              label: Text("4",
+                                  style: Theme.of(context).textTheme.button),
+                            ))),
+                  ],
+                )),
+            Padding(
+                padding: paddingVertical,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Padding(
+                        padding: paddingHorizontal,
+                        child: Transform.scale(
+                            scale: 2.0,
+                            child: FloatingActionButton.extended(
+                              heroTag: 3,
+                              hoverColor: Colors.red,
+                              onPressed: () => {
+                                game.setNumberOfPlayers(5),
+                                Navigator.popAndPushNamed(
+                                    context, '/setPlayerNames')
+                              },
+                              tooltip: '5 Spieler',
+                              label: Text(
+                                "5",
+                                style: Theme.of(context).textTheme.button,
+                              ),
+                            ))),
+                    Padding(
+                        padding: paddingHorizontal,
+                        child: Transform.scale(
+                            scale: 2.0,
+                            child: FloatingActionButton.extended(
+                              heroTag: 4,
+                              hoverColor: Colors.red,
+                              onPressed: () => {
+                                game.setNumberOfPlayers(6),
+                                Navigator.popAndPushNamed(
+                                    context, '/setPlayerNames')
+                              },
+                              tooltip: '6 Spieler',
+                              label: Text("6",
+                                  style: Theme.of(context).textTheme.button),
+                            ))),
+                  ],
+                )),
           ],
         ),
       ),
